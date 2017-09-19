8bb6012ab3f450c3296ae994a7d9f678fecc8056
Danny10X <dannyrc100@hotmail.com>
2017-09-18 20:23:16 -0400

    Work In Progress - Part 2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,48 @@
-alert("Test Test Test");
+//  Interval Demonstration
+//  Set our number counter to 60.
+var number = 60;
+
+//  Variable that will hold our interval ID when we execute
+//  the "run" function
+var intervalId;
+
+$("#start").on("click", number.start);
+
+//  The run function sets an interval
+//  that runs the decrement function once a second.
+function run() {
+  intervalId = setInterval(decrement, 1000);
+}
+
+//  The decrement function.
+function decrement() {
+
+  //  Decrease number by one.
+  number--;
+
+  //  Show the number in the #show-number tag.
+  $("#show-number").html("<p>" + number + "</p>");
+
+
+  //  Once number hits zero...
+  if (number === 0) {
+
+    //  ...run the stop function.
+    stop();
+
+    //  Alert the user that time is up.
+    alert("All Done!");
+  }
+}
+
+//  The stop function
+function stop() {
+
+  //  Clears our intervalId
+  //  We just pass the name of the interval
+  //  to the clearInterval function.
+  clearInterval(intervalId);
+}
+
+//  Execute the run function.
+run();
